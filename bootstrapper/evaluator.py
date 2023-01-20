ffe1c0d72c2a5a57bc45d6fa90e6c1b9aba9999f
Jake Hyun <jake.hyun@hotmail.com>
2023-01-20 18:33:53 +0900

    Begin bootstrapper AI implementation

---
+++ bootstrapper/evaluator.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as torchf
+import torch.optim as optim
+
+
+class Evaluator(nn.Module):
+    """Given the game state (pre-generation of new bricks), evaluate how close it is to game-over.
+
+    Input: brick layout (excluding top and bottom rows) [7x6] + ball count [1] == [43]
+    Output: log((steps to game-over) + 1)
+
+    This means that the output is always non-negative.
+    A larger output signifies a 'better' game state.
+    """
+
+    def __init__(self):
+        super().__init__()
+        dim_x = 6
+        dim_y = 9
+        h1 = 24
+        h2 = 12
+        self.lin1 = nn.Linear(dim_x * (dim_y - 2) + 1, h1)
+        self.drop1 = nn.Dropout(p=0.5)
+        self.lin2 = nn.Linear(h1, h2)
+        self.drop2 = nn.Dropout(p=0.5)
+        self.lin3 = nn.Linear(h2, 1)
+
+    def forward(self, x):
+        x = torchf.relu(self.drop1(self.lin1(x)))
+        x = torchf.relu(self.drop2(self.lin2(x)))
+        x = torchf.relu(self.lin3(x))
+        return x
+
